4186c5dc8112fabba9f8cd38d66256f6fd6570af
Krish Bhalala <145704986+Krish-Bhalala@users.noreply.github.com>
2025-01-13 23:57:22 -0600

    created masking feature and object bounding box on the webcam frames

--- color_detection.py
+++ color_detection.py
@@ -1,7 +1,12 @@
 import cv2
+from util import get_color_limits
+from PIL import Image
 
 # for VideoCapture(<webcam Number>) we will have to specify the webcam id, by default the main/primary webcam is assigned 0 and reast as 1,2,3,....
 webcam = cv2.VideoCapture(0)
+
+# the color in BGR to detect 
+COLOR = [255,0,0] 
 
 # Now we will display the frames/pictures captured by the webcam for approx every 1ms (usually faster than that)
 while True:
@@ -9,7 +14,27 @@ while True:
     ret, frame = webcam.read()
 
     # PROCESSING FRAME
+    # first we will convert frame to HSV to better filter out our color
+    hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
+    # now compute the hsv value range for creating mask
+    lowerLimit, upperLimit = get_color_limits(COLOR)
+
+    # now we will create the mask which filters out the color range 
+    mask = cv2.inRange(hsv_img, lowerLimit, upperLimit)
+
+    # now convert the image array back to array 
+    mask_img = Image.fromarray(mask)
+
+    # now create a box around the detected object mask 
+    box = mask_img.getbbox()
+
+    # now draw the box on the img 
+    if box is not None:
+        x1,y1,x2,y2 = box
+        cv2.rectangle(frame, (x1,y1), (x2,y2), (255,0,255), 5)
+
+
     # DISPLAYING FRAME 
     cv2.imshow("processed frame", frame)
 
